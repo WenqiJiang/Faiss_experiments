b6c78ed5295aa99b4da5bde7c29e4d443bb389c7
Wenqi Jiang <wejiang@r630-04.ethz.ch>
2021-12-15 12:52:02 +0100

    update CPU recall dict

--- bench_cpu_performance.py
+++ bench_cpu_performance.py
@@ -232,8 +232,9 @@ if not args.load_from_dict: # Mode A: using arguments passed by the arguments
         ## Wenqi: batch size = 1
         # D, I = index.search(query_vec, 10)
         t1 = time.time()
-        n_ok = (I[:, :topK] == gt[:, :1]).sum()
-        print("%.4f" % (n_ok / float(nq)), end=' ')
+
+        #n_ok = (I[:, :topK] == gt[:, :1]).sum()
+        #print("%.4f" % (n_ok / float(nq)), end=' ')
         #for rank in 1, 10:
         #    n_ok = (I[:, :rank] == gt[:, :1]).sum()
         #    print("%.4f" % (n_ok / float(nq)), end=' ')
