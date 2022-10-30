aae0d854b86422aaa60e931ee9cb0b58fe94b3a7
wenqijiang <wenqi.j@columbia.edu>
2022-10-30 16:58:38 +0100

    fix latency bound

--- bench_multi_cpu_performance_OSDI.py
+++ bench_multi_cpu_performance_OSDI.py
@@ -253,8 +253,8 @@ for qbs in qbs_list:
         total_sec = np.sum(tail_latency_ms) / 1000
         real_QPS = 10000 / total_sec
         sorted_t_query_list = np.sort(np.array(tail_latency_ms))
-        latency_50 = sorted_t_query_list[int(np.ceil(len(sorted_t_query_list) * 0.5))]
-        latency_95 = sorted_t_query_list[int(np.ceil(len(sorted_t_query_list) * 0.95))]
+        latency_50 = sorted_t_query_list[np.amin([len(sorted_t_query_list), int(np.ceil(len(sorted_t_query_list) * 0.5))])]
+        latency_95 = sorted_t_query_list[np.amin([len(sorted_t_query_list), int(np.ceil(len(sorted_t_query_list) * 0.95))])]
 
         dict_perf_merged[dbname][index_key][qbs][nprobe]["QPS"] = real_QPS
         dict_perf_merged[dbname][index_key][qbs][nprobe]["latency@50"] = latency_50 # in ms
