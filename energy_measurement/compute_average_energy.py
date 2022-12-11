69a904470af3cd3495bcadd58b437f1f7b2249a2
Ubuntu <ubuntu@ip-172-31-36-78.ec2.internal>
2022-12-11 10:10:54 +0000

    update SIFT1000M energy

--- energy_measurement/compute_average_energy.py
+++ energy_measurement/compute_average_energy.py
@@ -25,6 +25,19 @@ Example script:
 | 39%   74C    P2   226W / 250W |  10853MiB / 12288MiB |     81%      Default |
 |     
 
+Another example:
+
++-----------------------------------------------------------------------------+
+| NVIDIA-SMI 520.61.05    Driver Version: 520.61.05    CUDA Version: 11.8     |
+|-------------------------------+----------------------+----------------------+
+| GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
+| Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
+|                               |                      |               MIG M. |
+|===============================+======================+======================|
+|   0  Tesla V100-SXM2...  On   | 00000000:00:16.0 Off |                    0 |
+| N/A   46C    P0   178W / 300W |   4285MiB / 32768MiB |     91%      Default |
+|                               |                      |                  N/A |
+
 """
 
 with open(dir_in, 'r') as f:
@@ -32,9 +45,11 @@ with open(dir_in, 'r') as f:
 
 energy_consumption = []
 for i, line in enumerate(file):
-    ID_match_str = "{}  NVIDIA".format(str(gpu_id))
-    match = re.search(ID_match_str, line) # next line is energy
-    if match:
+    ID_match_str_1 = "{}  Tesla".format(str(gpu_id))
+    match_1 = re.search(ID_match_str_1, line) # next line is energy
+    ID_match_str_2 = "{}  NVIDIA".format(str(gpu_id))
+    match_2 = re.search(ID_match_str_2, line) # next line is energy
+    if match_1 or match_2:
         if i + 1 < len(file):
             str_list = re.split(' ', file[i + 1])
             for s in str_list:
